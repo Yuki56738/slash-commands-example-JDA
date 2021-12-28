23fd0703d5150d2469b33041c0c3eed67fcddb7a
Yuki <me@example.com>
2021-12-28 20:57:09 +0900

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,39 @@ import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 
 import static java.lang.System.out;
 
-public class Main {
+public class Main extends ListenerAdapter {
     public static void main(String[] args) throws LoginException, InterruptedException {
         Dotenv dotenv = Dotenv.load();
         JDA jda = JDABuilder.createDefault(dotenv.get("DISCORD_TOKEN"))
                 .setActivity(Activity.playing("type /yuki"))
-                .setRawEventsEnabled(true)
+//                .setRawEventsEnabled(true)
+                .addEventListeners(new Main())
                 .build();
         jda.awaitReady();
         out.println("Bot is ready.");
+        jda.updateCommands().queue();
 
+        jda.upsertCommand("yuki", "Reply with Pong!").queue();
     }
 
+    @Override
+    public void onSlashCommand(SlashCommandEvent event) {
+        out.println("onSlashCommand hit.");
+        out.println(event.getName());
+
+        if (event.getName().equalsIgnoreCase("yuki")) {
+            event.reply("Pong!").setEphemeral(false).queue();
+        } else {
+            event.reply("Invalid command.").setEphemeral(false).queue();
+        }
+    }
 }
+
+
